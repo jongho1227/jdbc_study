4f46ae898cd4f8591b4dcfb960a55e44627bb222
임종호 <ejsvkvhdlsxm@naver.com>
2019-06-21 15:46:50 +0900

    interface DeptEmpTransactionDao 추가

--- src/jdbc_study/ErpMain.java
+++ src/jdbc_study/ErpMain.java
@@ -8,6 +8,7 @@ public class ErpMain {
 
 	public static void main(String[] args) {
 		System.out.println("첫 번째 수정");
+		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
