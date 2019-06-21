9726713083241748e793905a6517a5f329a2146f
임종호 <ejsvkvhdlsxm@naver.com>
2019-06-21 15:26:56 +0900

    Main수정

--- src/jdbc_study/ErpMain.java
+++ src/jdbc_study/ErpMain.java
@@ -7,6 +7,7 @@ import jdbc_study.ui.ErpManagementUI;
 public class ErpMain {
 
 	public static void main(String[] args) {
+		System.out.println("첫 번째 수정");
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
